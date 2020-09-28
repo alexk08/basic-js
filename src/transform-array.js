0280e921cfbe518c3f92acb6378c9e8909c2de10
alexk08 <akapacuk@gmail.com>
2020-09-28 10:17:40 +0400

    fix: change solution of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,42 +7,47 @@ module.exports = function transform(initArr) {
     if (initArr.length === 0) {
       return [];
     }
-    let transformArr = [];
+    let transformArr = initArr.slice();
   
-    for (let i = 0; i < initArr.length; i++) {
-      if (initArr[i] === '--double-next') {
-        if (i+1 !== initArr.length) {
-          initArr.splice((i), 1, initArr[i+1]);
+    for (let i = 0; i < transformArr.length; i++) {
+      if (transformArr[i] === '--double-next') {
+        if (i+1 !== transformArr.length) {
+          transformArr.splice((i), 1, transformArr[i+1]);
         } else {
-          initArr.splice((i), 1);
+          transformArr.splice((i), 1);
         }
-      } else if (initArr[i] === '--double-prev') {
+      } else if (transformArr[i] === '--double-prev') {
         if (i !== 0) {
-          initArr.splice((i), 1, initArr[i-1]);
+          transformArr.splice((i), 1, transformArr[i-1]);
         } else {
-          initArr.splice((i), 1);
+          transformArr.splice((i), 1);
         }
       } 
-    }
+    // }
   
-    for (let i = 0; i < initArr.length; i++) {
-      if (initArr[i] === '--discard-next') {
-        if (i+1 !== initArr.length) {
-          initArr.splice((i), 2);
-          i = i-1;
+    // for (let i = 0; i < transformArr.length; i++) {
+      else if (transformArr[i] === '--discard-next') {
+        if (i+1 !== transformArr.length) {
+          transformArr.splice((i), 2);
+        //   i = i-1;
         } else {
-          initArr.splice((i), 1);
+          transformArr.splice((i), 1);
         }
-      } else if (initArr[i] === '--discard-prev') {
+      } else if (transformArr[i] === '--discard-prev') {
         if (i !== 0) {
-          initArr.splice((i-1), 2);
-          i = i-1;
+          transformArr.splice((i-1), 2);
+        //   i = i-1;
         } else {
-          initArr.splice((i), 1);
+          transformArr.splice((i), 1);
         }
       }
     }
-  
-    transformArr = initArr;
+
+    for (let i = 0; i < transformArr.length; i++) {
+        if ((transformArr[i] === '--double-next') || (transformArr[i] === '--double-prev') || (transformArr[i] === '--discard-next') || (transformArr[i] === '--discard-prev')) {
+            transformArr.splice((i), 1);
+        }
+    }
+    // console.log(initArr);
     return transformArr;
   };
